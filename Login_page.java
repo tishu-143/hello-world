4a88ade139035acba258eca0e755de5b67dec631
tishu-143 <tishuthegreat@gmail.com>
2017-08-15 17:36:49 +0530

    Add files via upload

---
+++ Login_page.java
@@ -0,0 +1,14 @@
+package com.example.tishantghode.griet_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class Login_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_page);
+    }
+}
